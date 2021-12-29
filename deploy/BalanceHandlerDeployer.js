e961229f9277b0c2fff97421843251d1ece96ffa
Manmit Singh <ms785@duke.edu>
2021-12-29 17:36:34 -0500

    Almost all of the changes

---
+++ deploy/BalanceHandlerDeployer.js
@@ -0,0 +1,42 @@
+let { networkConfig } = require('../hardhat-helper-config')
+const fs = require('fs')
+
+require("dotenv").config();
+
+module.exports = async ({
+    getNamedAccounts,
+    deployments,
+    getChainId
+}) => {
+
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = await getChainId()
+
+    const accounts = await hre.ethers.getSigners()
+    const signer = accounts[0]
+
+    const FQONE_TOKEN = process.env.FQONE_TOKEN;
+    const BLUEBLOCK_TOKEN = process.env.BLUEBLOCK_TOKEN;
+
+    log("----------------------------------------------------")
+
+    console.log(FQONE_TOKEN)
+
+    const BalanceHandlerContract = await ethers.getContractFactory("BalanceHandler")
+    const balanceHandlerContract = await BalanceHandlerContract.deploy(FQONE_TOKEN)
+    await balanceHandlerContract.deployed()
+
+    log(`Contract has been deployed at the address ${balanceHandlerContract.address}`)
+
+    let balance_BN = await balanceHandlerContract.checkBalance("0x451A3C5ae32A0800Ef2668Ceb07DFC294fd43775");
+    let balance = balance_BN.toNumber()
+
+    console.log(`User has a balance of ${balance}`)
+
+    await balanceHandlerContract.rewardStudent("0x451A3C5ae32A0800Ef2668Ceb07DFC294fd43775", 100);
+
+    console.log(`Money is sent`)
+
+}
+module.exports.tags = ['all', 'svg']
